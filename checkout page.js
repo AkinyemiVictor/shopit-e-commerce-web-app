f47286c447a8184b3459582ff0680d550eaccc29
[Victor Akinyemi] <[victorakinyemi52@gmail.com]>
2025-01-25 09:45:47 +0100

    chack out page changes

--- checkout page.js
+++ checkout page.js
@@ -1,19 +1,18 @@
-const cart = document.getElementById('cart');
-const popup = document.getElementById('checkoutButton');
-const closePop = document.getElementById('closePop');
+const cart = document.getElementById("cart");
+const popup = document.getElementById("checkoutButton");
+const closePop = document.getElementById("closePop");
 popup.addEventListener("click", success);
 
-function success () {
-        document.getElementById('popup').style.display = "flex";
+function success() {
+  document.getElementById("popup").style.display = "flex";
 }
 
 closePop.addEventListener("click", close);
 
-function close () {
-    document.getElementById('popup').style.display = "none";
+function close() {
+  document.getElementById("popup").style.display = "none";
 }
 
-function navigatetoCart () {
-    window.location.href = "checkout page.html";
+function navigatetoCart() {
+  window.location.href = "checkout page.html";
 }
-
